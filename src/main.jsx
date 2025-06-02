700347a9f1f052b8ec56e6a22e4ff7023dfde227
bcheban <128683111+bcheban@users.noreply.github.com>
2025-06-02 15:23:35 +0300

    changed git ignore file

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/React-Movie">
+    <BrowserRouter basename="/react-movie">
       <App />
     </BrowserRouter>
   </StrictMode>
