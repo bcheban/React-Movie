731f28c8bd7a644242158b6e2fcc3022d58c2531
bcheban <128683111+bcheban@users.noreply.github.com>
2025-06-02 12:45:04 +0300

    created movie app in React

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "41468b3314aa6cdd07eee2f6acd15633";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
