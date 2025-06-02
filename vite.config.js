683ac51c4eb4c4e1aa5763a0810088bd30a08e73
bcheban <128683111+bcheban@users.noreply.github.com>
2025-06-02 15:13:47 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-movie/', // Ensure this matches your GitHub Pages repository name
+  base: '/react-movie/', // Ensure this matches your GitHub Pages repository Name
 })
