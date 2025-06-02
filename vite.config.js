d841301a2a6d2e8fa209cb8873c3bd3e4f1a870c
bcheban <128683111+bcheban@users.noreply.github.com>
2025-06-02 14:30:34 +0300

    trying to fix the problem with github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- base: '/React-Movie/', 
+  base: '/React-Movie/', // Ensure this matches your GitHub Pages repository name
 })
